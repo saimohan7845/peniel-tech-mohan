9caf996e55baf3ee6523438e1c82b86b75f6a3ef
saimohan7845 <91629385+saimohan7845@users.noreply.github.com>
2023-06-27 12:37:56 +0530

    Add files via upload

---
+++ src/pages/Presentation/index.js
@@ -0,0 +1,198 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+import MKSocialButton from "components/MKSocialButton";
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+import DefaultFooter from "examples/Footers/DefaultFooter";
+import FilledInfoCard from "examples/Cards/InfoCards/FilledInfoCard";
+import Counters from "pages/Presentation/sections/Counters";
+import Information from "pages/Presentation/sections/Information";
+import DesignBlocks from "pages/Presentation/sections/DesignBlocks";
+import Pages from "pages/Presentation/sections/Pages";
+import Testimonials from "pages/Presentation/sections/Testimonials";
+import Download from "pages/Presentation/sections/Download";
+import BuiltByDevelopers from "pages/Presentation/components/BuiltByDevelopers";
+import routes from "routes";
+import footerRoutes from "footer.routes";
+import bgImage from "assets/images/bg-presentation.jpg";
+
+function Presentation() {
+  return (
+    <>
+      <DefaultNavbar
+        routes={routes}
+        action={{
+          type: "external",
+          route: "/pages/get-started",
+          label: "get started",
+          color: "error",
+        }}
+        sticky
+      />
+      <MKBox
+        minHeight="75vh"
+        width="100%"
+        sx={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "top",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <>
+          <Grid container item xs={12} lg={7} justifyContent="center" mx="auto">
+            <MKTypography
+              variant="h1"
+              color="white"
+              mt={-6}
+              mb={1}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              Complete Accounting and IT Solutions.
+            </MKTypography>
+            <MKTypography
+              variant="body1"
+              color="white"
+              textAlign="left"
+              px={{ xs: 6, lg: 12 }}
+              mt={1}
+            >
+              Accounting software that manages your finances, keeps you VAT compliant, automates
+              business workflows, and helps you work collectively across departments. A one-stop-hub
+              for all your Accounting, ERP, CRM and Payroll.
+            </MKTypography>
+          </Grid>
+        </>
+      </MKBox>
+      <Card
+        sx={{
+          p: 2,
+          mx: { xs: 2, lg: 3 },
+          mt: -8,
+          mb: 4,
+          backgroundColor: ({ palette: { white }, functions: { rgba } }) => rgba(white.main, 0.8),
+          backdropFilter: "saturate(200%) blur(30px)",
+          boxShadow: ({ boxShadows: { xxl } }) => xxl,
+        }}
+      >
+        <Counters />
+        <Information />
+        <DesignBlocks />
+        <Pages />
+        <Container sx={{ mt: 6 }}>
+          <BuiltByDevelopers />
+        </Container>
+        <Container sx={{ mt: 6 }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} lg={4}>
+              <FilledInfoCard
+                variant="gradient"
+                color="info"
+                icon="flag"
+                title="Our Vision"
+                description="Our objective as an organization is to establish an expanded multinational customer base. We hope to accomplish by providing the utmost quality of products and services in tune with customer expectations. We work in collaboration with the customer by providing cost-effective solutions on time and prosper to exceed customer outlooks"
+                action={{
+                  type: "external",
+                  route: "/",
+                  label: "Let's start",
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} lg={4}>
+              <FilledInfoCard
+                color="info"
+                icon="precision_manufacturing"
+                title="Our Mission"
+                description="We believe that providing an excellent quality of service to our customers that endeavor to create a long-lasting relationship through our products and services. Keeping clients ahead of the competition. We work in collaboration with the customer. leverage on technical and managerial capabilities for providing a practical solution to the clients."
+                action={{
+                  type: "external",
+                  route: "/",
+                  label: "Read more",
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} lg={4}>
+              <FilledInfoCard
+                color="info"
+                icon="apps"
+                title="Our Goal"
+                description="Peniel Technology LLC has been conceptualized to serve the accounting needs of a diverse range of businesses and practicing as well. we provide you with a one-stop solution from a compliance and poise to handle your accounting process with absolute ease and ample time to emphasize on other aspects of your business."
+                action={{
+                  type: "external",
+                  route: "/",
+                  label: "Read more",
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+        <Testimonials />
+        <Download />
+        <MKBox pt={14} pb={6}>
+          <Container>
+            <Grid container spacing={3}>
+              <Grid item xs={12} lg={5} ml="auto" sx={{ textAlign: { xs: "center", lg: "left" } }}>
+                <MKTypography variant="h4" fontWeight="bold" mb={0.5}>
+                  Thank you for your support!
+                </MKTypography>
+                <MKTypography variant="body1" color="text">
+                  We deliver the best web products
+                </MKTypography>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                lg={5}
+                my={{ xs: 5, lg: "auto" }}
+                mr={{ xs: 0, lg: "auto" }}
+                sx={{ textAlign: { xs: "center", lg: "right" } }}
+              >
+                <MKSocialButton
+                  component="a"
+                  href="https://twitter.com/"
+                  target="_blank"
+                  color="twitter"
+                  sx={{ mr: 1 }}
+                >
+                  <i className="fab fa-twitter" />
+                  &nbsp;Tweet
+                </MKSocialButton>
+                <MKSocialButton
+                  component="a"
+                  href="https://www.facebook.com/"
+                  target="_blank"
+                  color="facebook"
+                  sx={{ mr: 1 }}
+                >
+                  <i className="fab fa-facebook" />
+                  &nbsp;Share
+                </MKSocialButton>
+                <MKSocialButton
+                  component="a"
+                  href="https://www.pinterest.com/pin/"
+                  target="_blank"
+                  color="pinterest"
+                >
+                  <i className="fab fa-pinterest" />
+                  &nbsp;Pin it
+                </MKSocialButton>
+              </Grid>
+            </Grid>
+          </Container>
+        </MKBox>
+      </Card>
+      <MKBox pt={6} px={1} mt={6}>
+        <DefaultFooter content={footerRoutes} />
+      </MKBox>
+    </>
+  );
+}
+
+export default Presentation;
